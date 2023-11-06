cc6304b115743d54ab41122a0be030e3ed05c8d6
KristianEkman <kristian.ekman.swe@gmail.com>
2023-11-06 08:46:20 +0100

    Add project files.

---
+++ Considition2023-Cs/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Considition2023_Cs
+{
+    internal static class HelperExtensions
+    {
+        public static void PrintJson(this object obj) { 
+            Console.WriteLine(JsonConvert.SerializeObject(obj, Formatting.Indented));
+        }
+    }
+}
